8bd955fee2a9f3f1db09ea36ff079603b52eda18
Jhon Esteban Correa Tabares <jhon.correa@globant.com>
2022-08-29 20:37:57 -0500

    Tarea 3 - Jhon Esteban Correa

---
+++ jhon-esteban/tarea3/oddOrEven.js
@@ -0,0 +1,11 @@
+function oddOrEven(array){
+    var test= 0
+    for (let i = 0; i < array.length; index++) {test = array[i];}
+    if (test %2 == 0){
+        return "even"
+    }
+    else
+    {
+        return "odd"
+    } 
+}
