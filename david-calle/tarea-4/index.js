51169991b08bbbfb41f963d9c7a8a7a4eb456a49
David Calle <davidcalleg@gmail.com>
2022-08-31 12:38:56 -0500

    ans, percentage, 0 operations, and more fixes implemented

--- david-calle/tarea-4/index.js
+++ david-calle/tarea-4/index.js
@@ -10,6 +10,7 @@ const eight = document.querySelector("[value='8']");
 const nine = document.querySelector("[value='9']");
 const clear = document.querySelector("[value='C']");
 const percentage = document.querySelector("[value='%']");
+const ansButton = document.querySelector("[value='ans']");
 const divide = document.querySelector("[value='/']");
 const multiply = document.querySelector("[value='x']");
 const minus = document.querySelector("[value='-']");
@@ -19,42 +20,62 @@ const decimalButton = document.querySelector("[value='.']");
 const summary = document.querySelector("#summary p");
 const input = document.querySelector("#input p");
 
-let firstOperand = 0;
-let secondOperand = 0;
-let operation = null;
 let isDecimal = false;
-let decimalValue = null;
+let isPercentage = false;
 let isResult = false;
+let isAns = false;
+let firstOperand = null;
+let secondOperand = null;
+let operation = null;
+let decimalValue = null;
 let additionalZeros = 0;
+let ans = 0;
 
-const turnToDecimal = (number) =>
-  number / 10 ** (number.toString().length + additionalZeros);
+const updateSummary = (data, reset = true) => {
+  if (reset) {
+    summary.innerHTML = data.toString().slice(-20);
+  } else {
+    summary.innerHTML += data.toString().slice(-20);
+  }
+};
+
+const updateInput = (data, reset = true) => {
+  if (reset) {
+    input.innerHTML = data.toString().substring(0, 10);
+  } else {
+    input.innerHTML += data.toString().substring(0, 10);
+  }
+};
+
+const turnToDecimal = (number) => {
+  if (!number) return 0;
+  return number / 10 ** (number.toString().length + additionalZeros);
+};
 
 const handleNumber = (number) => {
+  if (isAns || isPercentage) return;
   if (isResult) {
-    if (number === 0) return;
     clearAll();
     firstOperand = number;
     isResult = false;
   } else if (isDecimal) {
-    console.log(decimalValue);
     if (!decimalValue && number === 0) additionalZeros += 1;
     decimalValue = decimalValue * 10 + number;
   } else if (!operation) {
-    if (!firstOperand && number === 0) return;
-    firstOperand = (firstOperand ?? 0) * 10 + number;
+    if (firstOperand === 0 && number === 0) return;
+    firstOperand = firstOperand * 10 + number;
   } else {
-    if (!secondOperand && number === 0) return;
-    if (!secondOperand) summary.innerHTML += operation;
-    secondOperand = (secondOperand ?? 0) * 10 + number;
+    if (secondOperand === 0 && number === 0) return;
+    secondOperand = secondOperand * 10 + number;
   }
-  input.innerHTML += number;
-  summary.innerHTML += number;
+
+  if (input.innerHTML === operation) updateInput("");
+  updateInput(number, false);
+  updateSummary(number, false);
 };
 
 const handleDecimals = () => {
   if (!isDecimal) return;
-
   if (operation) {
     secondOperand += turnToDecimal(decimalValue);
   } else {
@@ -67,8 +88,7 @@ const handleDecimals = () => {
 };
 
 const solveOperation = () => {
-  if (!secondOperand) return;
-
+  if (secondOperand === null) return;
   switch (operation) {
     case "+":
       firstOperand += secondOperand;
@@ -83,42 +103,90 @@ const solveOperation = () => {
       firstOperand /= secondOperand;
       break;
   }
-  secondOperand = 0;
+  secondOperand = null;
   operation = null;
 };
 
 const handleOperation = (symbol) => {
-  if (operation && !secondOperand) return;
+  if (operation && secondOperand === null) return;
+  isAns = false;
   isResult = false;
+  isPercentage = false;
   handleDecimals();
   solveOperation();
-  input.innerHTML = symbol;
+  updateInput(symbol);
+  updateSummary(symbol, false);
   operation = symbol;
 };
 
 const handleDecimal = () => {
-  if (isDecimal) return;
-  input.innerHTML += ".";
-  summary.innerHTML += ".";
+  if (isDecimal || isAns) return;
+  if (isResult) clearAll();
+  if (input.innerHTML === "") {
+    updateInput("0", false);
+    updateSummary("0", false);
+    firstOperand = 0;
+  }
+  updateInput(".", false);
+  updateSummary(".", false);
   isDecimal = true;
 };
 
 const showResult = () => {
+  if (operation && secondOperand === null) return;
   handleDecimals();
   solveOperation();
-  input.innerHTML = firstOperand;
+  isAns = false;
+  isPercentage = false;
+  updateInput(firstOperand);
   isResult = true;
+  ans = firstOperand;
 };
 
 const clearAll = () => {
-  firstOperand = 0;
-  secondOperand = 0;
+  firstOperand = null;
+  secondOperand = null;
   operation = null;
   isDecimal = false;
   decimalValue = null;
+  isPercentage = false;
+  isAns = false;
   isResult = false;
-  input.innerHTML = "";
-  summary.innerHTML = "";
+  updateInput("");
+  updateSummary("");
+};
+
+const handleAns = () => {
+  if (!ans) return;
+  if (isResult) {
+    clearAll();
+    firstOperand = ans;
+    isResult = false;
+  } else if (!operation) {
+    if (firstOperand) return;
+    firstOperand = ans;
+  } else {
+    if (secondOperand) return;
+    secondOperand = ans;
+    updateSummary(operation, false);
+  }
+  isAns = true;
+  updateInput("ans");
+  updateSummary(ans, false);
+};
+
+const handlePercentage = () => {
+  if (isPercentage || isAns || isResult) return;
+  if (!operation) {
+    if (firstOperand === null) return;
+    firstOperand = firstOperand / 100;
+  } else {
+    if (secondOperand === null) return;
+    secondOperand = secondOperand / 100;
+  }
+  isPercentage = true;
+  updateInput("%", false);
+  updateSummary("%", false);
 };
 
 zero.addEventListener("click", () => handleNumber(0));
@@ -138,3 +206,5 @@ minus.addEventListener("click", () => handleOperation("-"));
 plus.addEventListener("click", () => handleOperation("+"));
 equals.addEventListener("click", showResult);
 clear.addEventListener("click", clearAll);
+ansButton.addEventListener("click", handleAns);
+percentage.addEventListener("click", handlePercentage);
