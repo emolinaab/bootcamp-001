0b058321cef78dd66b74426b001375e7c70d12ef
David Calle <davidcalleg@gmail.com>
2022-09-01 10:47:43 -0500

    movement to empty slots implemented

--- david-calle/tarea-5/index.js
+++ david-calle/tarea-5/index.js
@@ -16,6 +16,16 @@ const positions = [
   [2, 2],
 ];
 
+let piecePosition = null;
+
+let isDown = false;
+
+const containerPosition = document
+  .querySelector("#puzzle-container")
+  .getBoundingClientRect();
+
+const squareRadius = containerPosition.width / 6;
+
 const getDirection = (
   objectLeft,
   objectRight,
@@ -27,8 +37,8 @@ const getDirection = (
   const distances = {
     right: mouseX - objectRight,
     left: objectLeft - mouseX,
-    top: objectTop - mouseY,
-    bottom: mouseY - objectBottom,
+    up: objectTop - mouseY,
+    down: mouseY - objectBottom,
   };
 
   return Object.keys(distances).reduce((a, b) =>
@@ -44,17 +54,48 @@ const getMousePosition = () => {
   };
 };
 
-let piecePosition = null;
-let isDown = false;
+const move = (piece, direction) => {
+  const row = parseInt(piece.getAttribute("row"));
+  const col = parseInt(piece.getAttribute("col"));
+  let movex = 0;
+  let movey = 0;
+  switch (direction) {
+    case "right":
+      movex += 1;
+      break;
+    case "left":
+      movex += -1;
+      break;
+    case "up":
+      movey += -1;
+      break;
+    case "down":
+      movey += 1;
+      break;
+  }
+
+  const destinyRow = row + movey;
+  const destinyCol = col + movex;
+
+  if (destinyRow < 0 || destinyRow > 2 || destinyCol < 0 || destinyCol > 2) {
+    piece.style.top = (33.33 * row).toString() + "%";
+    piece.style.left = (33.33 * col).toString() + "%";
+  } else if (matrix[destinyRow][destinyCol] === null) {
+    matrix[row][col] = null;
+    matrix[destinyRow][destinyCol] = piece;
+    piece.style.top = (33.33 * destinyRow).toString() + "%";
+    piece.style.left = (33.33 * destinyCol).toString() + "%";
+    piece.setAttribute("row", destinyRow);
+    piece.setAttribute("col", destinyCol);
+  } else {
+    piece.style.top = (33.33 * row).toString() + "%";
+    piece.style.left = (33.33 * col).toString() + "%";
+  }
+};
 
 const handleMousemove = (event) => {
   if (!isDown) return;
-  const containerPosition = document
-    .querySelector("#puzzle-container")
-    .getBoundingClientRect();
-  const squareRadius = containerPosition.width / 6;
 
-  event.target.style.position;
   const mousePosition = getMousePosition();
   event.target.style.top =
     mousePosition.y - containerPosition.top - squareRadius + "px";
@@ -69,9 +110,11 @@ const handleMousedown = (event) => {
 };
 
 const handleMouseup = (event) => {
+  isDown = false;
   event.target.style["z-index"] = 0;
   const mousePosition = getMousePosition();
-  console.log(
+  move(
+    event.target,
     getDirection(
       piecePosition.left,
       piecePosition.right,
@@ -81,11 +124,6 @@ const handleMouseup = (event) => {
       mousePosition.y
     )
   );
-  event.target.style.top =
-    (33.33 * event.target.getAttribute("row")).toString() + "%";
-  event.target.style.left =
-    (33.33 * event.target.getAttribute("col")).toString() + "%";
-  isDown = false;
 };
 
 const pieces = document.querySelectorAll(".puzzle-piece");
