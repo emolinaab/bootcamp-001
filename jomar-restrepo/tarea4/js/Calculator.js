7310c4ebfc42e80551a51b2ebb6bc03067045c02
Leviathan9020 <jomar2030@hotmail.com>
2022-09-05 09:15:40 -0500

    cambios

---
+++ jomar-restrepo/tarea4/js/Calculator.js
@@ -0,0 +1,17 @@
+class Calculator {
+  toadd(num1, num2) {
+    return num1 + num2;
+  }
+
+  tosubtract(num1, num2) {
+    return num1 - num2;
+  }
+
+  todivide(num1, num2) {
+    return num1 / num2;
+  }
+
+  tomultiply(num1, num2) {
+    return num1 * num2;
+  }
+}
