1318f566d204167b10b1e487910edfd5463a496b
Sebastian Rendon <s.rendon@globant.com>
2022-09-16 15:27:13 -0500

    refactor: oop approach

--- sebastian-rendon/tarea-5/index.js
+++ sebastian-rendon/tarea-5/index.js
@@ -1,8 +1,7 @@
-let puzzle = {
-  gridSize: 3,
-  pieces: null,
-  element: null,
-};
+import Game from './js/game.js';
+import Puzzle from './js/puzzle.js';
+
+let game = new Game();
 
 function setup() {
   const sizeFormEl = document.getElementById('size-form');
@@ -17,12 +16,14 @@ function onSizeFormSubmit(evt) {
   const formData = new FormData(evt.target);
   const size = Number.parseInt(formData.get('size'));
 
-  createPuzzleMap(Number.isNaN(size) || size < 3 ? 3 : size);
-  mountPuzzleElement();
+  game = new Game(new Puzzle(size), document.getElementById('puzzle'));
+  game.puzzle.createPuzzleMap();
+  // game.puzzle.shuffle();
+  game.mountPuzzleElement();
 }
 
 function onPlayAgainBtnClick() {
-  const puzzleEl = puzzle.element;
+  const puzzleEl = game.element;
   puzzleEl.classList.remove('puzzle__body--visible');
 
   const formEl = document.getElementById('size-form');
@@ -30,158 +31,6 @@ function onPlayAgainBtnClick() {
 
   const victoryEl = document.getElementById('victory-message');
   victoryEl.classList.remove('victory--open');
-}
-
-function createPuzzleMap(squareDimension) {
-  puzzle = {
-    gridSize: squareDimension,
-    element: document.getElementById('puzzle'),
-    pieces: new Map(),
-  };
-
-  puzzle.pieces.set('empty', {
-    number: 0,
-    position: squareDimension * squareDimension - 1,
-  });
-
-  for (let i = 1; i < squareDimension * squareDimension; i++) {
-    puzzle.pieces.set(i, {
-      number: i,
-      position: i - 1,
-    });
-  }
-}
-
-function mountPuzzleElement() {
-  shuffle();
-  mountPuzzlePieceElements();
-
-  const puzzleEl = puzzle.element;
-  puzzleEl.classList.add('puzzle__body--visible');
-  puzzleEl.style.width = `${puzzle.gridSize * 5.5}rem`;
-  puzzleEl.style.height = `${puzzle.gridSize * 5.5}rem`;
-
-  const formEl = document.getElementById('size-form');
-  formEl.classList.add('size-form--hidden');
-}
-
-function mountPuzzlePieceElements() {
-  puzzle.element.innerHTML = '';
-  for (const [key, piece] of puzzle.pieces) {
-    const pieceEl = document.createElement('div');
-    pieceEl.setAttribute('data-key', piece.number);
-    translatePiece(piece, pieceEl);
-
-    if (key === 'empty') {
-      pieceEl.classList.add('puzzle__tile', 'puzzle__tile--empty');
-    } else {
-      pieceEl.innerHTML = `${piece.number}`;
-      pieceEl.classList.add('puzzle__tile');
-      pieceEl.addEventListener('click', (e) => updatePiecePosition(e.target));
-    }
-    puzzle.element.appendChild(pieceEl);
-  }
-}
-
-function updatePiecePosition(pieceEl) {
-  const pieceKey = Number.parseInt(pieceEl.dataset.key);
-  const emptyPieceEl = document.querySelector('.puzzle__tile--empty');
-
-  const piece = puzzle.pieces.get(pieceKey);
-  const emptyPiece = puzzle.pieces.get('empty');
-
-  if (canMovePiece(piece, emptyPiece)) {
-    exchangePositions(piece, emptyPiece);
-    translatePiece(piece, pieceEl);
-    translatePiece(emptyPiece, emptyPieceEl);
-
-    const victoryEl = document.getElementById('victory-message');
-    if (checkSolution()) {
-      victoryEl.classList.add('victory--open');
-    } else {
-      victoryEl.classList.remove('victory--open');
-    }
-  } else {
-    shake(piece, pieceEl);
-  }
-}
-
-function canMovePiece(piece, emptyPiece) {
-  const adjacentPositions = getAdjacentPositions(piece);
-
-  if (adjacentPositions.includes(emptyPiece.position)) return true;
-  return false;
-}
-
-function shake(piece, element) {
-  const gridSize = puzzle.gridSize;
-
-  element.style = `transform: translate(${
-    (piece.position % gridSize) * 5.5 - 0.25
-  }rem, ${Math.floor(piece.position / gridSize) * 5.5}rem)`;
-  setTimeout(() => {
-    element.style = `transform: translate(${
-      (piece.position % gridSize) * 5.5 + 0.25
-    }rem, ${Math.floor(piece.position / gridSize) * 5.5}rem)`;
-  }, 200);
-  setTimeout(() => {
-    element.style = `transform: translate(${
-      (piece.position % gridSize) * 5.5
-    }rem, ${Math.floor(piece.position / gridSize) * 5.5}rem)`;
-  }, 400);
-}
-
-function exchangePositions(piece1, piece2) {
-  const piece1Pos = piece1.position;
-  const piece2Pos = piece2.position;
-
-  piece2.position = piece1Pos;
-  piece1.position = piece2Pos;
-}
-
-function translatePiece(piece, element) {
-  const gridSize = puzzle.gridSize;
-  element.style = `transform: translate(${
-    (piece.position % gridSize) * 5.5
-  }rem, ${Math.floor(piece.position / gridSize) * 5.5}rem)`;
-  element.setAttribute('data-pos', piece.position);
-}
-
-function getAdjacentPositions(originPiece) {
-  const gridSize = puzzle.gridSize;
-  const adjacentPositions = [];
-
-  if (originPiece.position % gridSize !== 0) {
-    adjacentPositions.push(originPiece.position - 1);
-  }
-  if (originPiece.position % gridSize !== gridSize - 1) {
-    adjacentPositions.push(originPiece.position + 1);
-  }
-  if (originPiece.position / gridSize >= 1) {
-    adjacentPositions.push(originPiece.position - gridSize);
-  }
-  if (originPiece.position / gridSize <= gridSize - 1) {
-    adjacentPositions.push(originPiece.position + gridSize);
-  }
-  return adjacentPositions;
-}
-
-function shuffle() {
-  const randomPositions = Array.from(puzzle.pieces.values())
-    .map((piece) => piece.position)
-    .sort(() => Math.random() - 0.5);
-
-  for (const [_key, piece] of puzzle.pieces) {
-    piece.position = randomPositions.pop();
-  }
-}
-
-function checkSolution() {
-  for (const [_key, piece] of puzzle.pieces) {
-    if (piece.number === 0) continue;
-    if (piece.position !== piece.number - 1) return false;
-  }
-  return true;
 }
 
 window.onload = setup;
---
+++ sebastian-rendon/tarea-5/js/puzzle.js
@@ -0,0 +1,78 @@
+export default class Puzzle {
+  /**
+   *
+   * @param {number} gridSize
+   * @param {Map<string|number, Object>} pieces
+   */
+  constructor(gridSize) {
+    this.gridSize = Number.isNaN(gridSize) ? 3 : Math.max(3, gridSize);
+    this.pieces = new Map();
+  }
+
+  createPuzzleMap() {
+    this.pieces.set('empty', {
+      number: 0,
+      position: this.gridSize * this.gridSize - 1,
+    });
+
+    for (let i = 1; i < this.gridSize * this.gridSize; i++) {
+      this.pieces.set(i, {
+        number: i,
+        position: i - 1,
+      });
+    }
+    console.log(this.pieces);
+  }
+
+  shuffle() {
+    const randomPositions = Array.from(this.pieces.values())
+      .map((piece) => piece.position)
+      .sort(() => Math.random() - 0.5);
+
+    for (const [_key, piece] of this.pieces) {
+      piece.position = randomPositions.pop();
+    }
+  }
+
+  checkSolution() {
+    for (const [_key, piece] of this.pieces) {
+      if (piece.number === 0) continue;
+      if (piece.position !== piece.number - 1) return false;
+    }
+    return true;
+  }
+
+  exchangePositions(piece1, piece2) {
+    const piece1Pos = piece1.position;
+    const piece2Pos = piece2.position;
+
+    piece2.position = piece1Pos;
+    piece1.position = piece2Pos;
+  }
+
+  canMovePiece(piece, emptyPiece) {
+    const adjacentPositions = this.getAdjacentPositions(piece);
+
+    if (adjacentPositions.includes(emptyPiece.position)) return true;
+    return false;
+  }
+
+  getAdjacentPositions(originPiece) {
+    const gridSize = this.gridSize;
+    const adjacentPositions = [];
+
+    if (originPiece.position % gridSize !== 0) {
+      adjacentPositions.push(originPiece.position - 1);
+    }
+    if (originPiece.position % gridSize !== gridSize - 1) {
+      adjacentPositions.push(originPiece.position + 1);
+    }
+    if (originPiece.position / gridSize >= 1) {
+      adjacentPositions.push(originPiece.position - gridSize);
+    }
+    if (originPiece.position / gridSize <= gridSize - 1) {
+      adjacentPositions.push(originPiece.position + gridSize);
+    }
+    return adjacentPositions;
+  }
+}
