1c52909988daf03350000b6ec9e0620aa1cbafb9
David Calle <davidcalleg@gmail.com>
2022-09-20 18:31:40 -0500

    improving tests

--- david-calle/tarea-8/src/__tests__/Form.test.js
+++ david-calle/tarea-8/src/__tests__/Form.test.js
@@ -1,45 +1,55 @@
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import {
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+  cleanup,
+} from "@testing-library/react";
 import Form from "../components/Form";
 
+let signInButton = null;
+let usernameInput = null;
+let passwordInput = null;
+let container = null;
+
+beforeEach(() => {
+  ({ container } = render(<Form />));
+  signInButton = screen.getByText(/Sign In/i, { selector: "input" });
+  usernameInput = screen.getByLabelText(/Username/i, {
+    selector: "input",
+  });
+  passwordInput = screen.getByLabelText(/Password/i, {
+    selector: "input",
+  });
+});
+
+afterEach(() => {
+  signInButton = null;
+  usernameInput = null;
+  passwordInput = null;
+  container = null;
+  cleanup();
+});
+
 it("keeps sign in button disabled if username or password are not valid", () => {
-  render(<Form />);
-  const signInButton = screen.getByText(/Sign In/i, { selector: "input" });
-  const usernameInput = screen.getByLabelText(/Username/i, {
-    selector: "input",
-  });
-  const passwordInput = screen.getByLabelText(/Password/i, {
-    selector: "input",
-  });
-  fireEvent.change(usernameInput, { target: { value: "Dcg" } });
-  fireEvent.change(passwordInput, { target: { value: "qwe123" } });
+  const invalidUsername = "Dcg";
+  const invalidPassword = "qwe123";
+  fireEvent.change(usernameInput, { target: { value: invalidUsername } });
+  fireEvent.change(passwordInput, { target: { value: invalidPassword } });
   expect(signInButton.disabled).toBeTruthy();
 });
 
 it("enables sign in button if username and password are valid", () => {
-  render(<Form />);
-  const signInButton = screen.getByText(/Sign In/i, { selector: "input" });
-  const usernameInput = screen.getByLabelText(/Username/i, {
-    selector: "input",
-  });
-  const passwordInput = screen.getByLabelText(/Password/i, {
-    selector: "input",
-  });
-  fireEvent.change(usernameInput, { target: { value: "Davidcalle1" } });
-  fireEvent.change(passwordInput, { target: { value: "Queop_123" } });
+  const validUsername = "Davidcalle1";
+  const validPassword = "Queop_123";
+  fireEvent.change(usernameInput, { target: { value: validUsername } });
+  fireEvent.change(passwordInput, { target: { value: validPassword } });
   expect(signInButton.disabled).toBeFalsy();
 });
 
 it("shows the logged in message if user has successfully logged in", async () => {
-  render(<Form />);
   const correctUsername = "juanchoman1";
   const correctPassword = "Qweop_123";
-  const signInButton = screen.getByText(/Sign In/i, { selector: "input" });
-  const usernameInput = screen.getByLabelText(/Username/i, {
-    selector: "input",
-  });
-  const passwordInput = screen.getByLabelText(/Password/i, {
-    selector: "input",
-  });
   fireEvent.change(usernameInput, { target: { value: correctUsername } });
   fireEvent.change(passwordInput, { target: { value: correctPassword } });
   fireEvent.click(signInButton);
@@ -51,16 +61,8 @@ it("shows the logged in message if user has successfully logged in", async () =>
 });
 
 it("shows an error message if username or password are incorrect", async () => {
-  const { container } = render(<Form />);
   const incorrectUsername = "testUser1";
   const correctPassword = "Qweop_123";
-  const signInButton = screen.getByText(/Sign In/i, { selector: "input" });
-  const usernameInput = screen.getByLabelText(/Username/i, {
-    selector: "input",
-  });
-  const passwordInput = screen.getByLabelText(/Password/i, {
-    selector: "input",
-  });
   fireEvent.change(usernameInput, { target: { value: incorrectUsername } });
   fireEvent.change(passwordInput, { target: { value: correctPassword } });
   fireEvent.click(signInButton);
--- david-calle/tarea-8/src/__tests__/utils.test.js
+++ david-calle/tarea-8/src/__tests__/utils.test.js
@@ -1,23 +1,23 @@
 import { checkValidAccount, signIn } from "../utils";
 
 test("checkValidAccount checks correctly if the username and password are valid", () => {
-  const validtUsername = "DavidCalleg1";
-  const validtPassword = "Qeop123_qwe";
-  const result = checkValidAccount(validtUsername, validtPassword);
+  const validUsername = "DavidCalleg1";
+  const validPassword = "Qeop123_qwe";
+  const result = checkValidAccount(validUsername, validPassword);
   expect(result).toBeTruthy();
 });
 
 test("checkValidAccount returns false if username is invalid", () => {
-  const invalidtUsername = "_dc";
-  const validtPassword = "132145asda";
-  const result = checkValidAccount(invalidtUsername, validtPassword);
+  const invalidUsername = "_dc";
+  const validPassword = "132145asda";
+  const result = checkValidAccount(invalidUsername, validPassword);
   expect(result).toBeFalsy();
 });
 
 test("checkValidAccount returns false if password is invalid", () => {
-  const validtUsername = "Hernan_122";
-  const invalidtPassword = "q1";
-  const result = checkValidAccount(validtUsername, invalidtPassword);
+  const validUsername = "Hernan_122";
+  const invalidPassword = "q1";
+  const result = checkValidAccount(validUsername, invalidPassword);
   expect(result).toBeFalsy();
 });
 
@@ -27,19 +27,19 @@ test("signIn works correctly for a registered user", async () => {
   await expect(signIn(correctUsername, correctPassword)).resolves.toEqual(true);
 });
 
-test("signIn returns false for an unregistered user", async () => {
+test("signIn rejects for an unregistered user", async () => {
   const unregisteredUsername = "DHenaoo1";
   const password = "12345678*";
   await expect(signIn(unregisteredUsername, password)).rejects.toThrow(Error);
 });
 
-test("signIn returns false if password is not valid", async () => {
+test("signIn rejects if password is not valid", async () => {
   const validUsername = "user_12324";
   const invalidPassword = "132";
   await expect(signIn(validUsername, invalidPassword)).rejects.toThrow(Error);
 });
 
-test("signIn returns false if username is not valid", async () => {
+test("signIn rejects if username is not valid", async () => {
   const invalidUsername = "usr";
   const validPassword = "132234587";
   await expect(signIn(invalidUsername, validPassword)).rejects.toThrow(Error);
