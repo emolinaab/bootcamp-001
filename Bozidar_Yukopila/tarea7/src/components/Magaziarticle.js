18952c6330deb844ac04e4c703cc5bcfa63a7532
Bozidary24 <byukopila@uninorte.edu.co>
2022-09-22 02:25:41 -0500

    Tarea 7

---
+++ Bozidar_Yukopila/tarea7/src/components/Magaziarticle.js
@@ -0,0 +1,15 @@
+import React  from "react";
+function Magaziarticle() {
+    return (
+        <article className="information">
+
+        <p className="text2" tabindex="6">THE CONFIRMED LIST OF XBOX GAME PASS</p>
+        <p className="text3" tabindex="7">PLAY AND EARN MONEY WITH XBOX</p>
+        <p className="text4" tabindex="8">NEW XBOX DEVICES FOR VIRTUAL REALITY</p>
+
+    </article>
+    ) ;
+  
+
+}
+export default Magaziarticle ; 
