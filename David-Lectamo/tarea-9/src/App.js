0bc1fff6964d7457192f0e32159e54ec2b5c6178
L3C74M0 <juandavidlectamocaicedo@gmail.com>
2022-09-23 10:33:09 -0500

    Fixed bug that didn't allow to advance to the next question without clicking submit twice.

--- David-Lectamo/tarea-9/src/App.js
+++ David-Lectamo/tarea-9/src/App.js
@@ -5,51 +5,51 @@ import { useState } from 'react';
 let attempt = 1;
 
 function App() {
-  const [movie, setMovie] = useState('Click en send to start');
-  const [success, setSuccess] = useState(0);
+  const [movie, setMovie] = useState('🌎🐒🐵🦍');
   const [count, setCount] = useState(0);
-  const [lives, setLives] = useState(20);
+  const [lives, setLives] = useState(10);
   
 
   const TakeAnswer = answer => {
     const str = answer.toLowerCase();
-    if (str === 'el planeta de los simios' && attempt === 1) {
-      setSuccess(1);
-      attempt = 2;
-      setCount(count+1);
-    } else if (attempt === 1) {
-      setLives(lives-1);
-    }
 
-    if (str === 'angeles y demonios' && attempt === 2) {
-      setSuccess(2);
-      attempt = 3;
+    if (str === 'el señor de los anillos' && attempt === 5) {
+      attempt = 6;
       setCount(count+1);
-    } else if (attempt === 2) {
-      setLives(lives-1);
-    }
-
-    if (str === 'titanic' && attempt === 3) {
-      setSuccess(3);
-      attempt = 4;
-      setCount(count+1);
-    } else if (attempt === 3) {
+      setMovie('🏁');
+    } else if (attempt === 5) {
       setLives(lives-1);
     }
 
     if (str === 'the ring' && attempt === 4) {
-      setSuccess(4);
       attempt = 5;
       setCount(count+1);
+      setMovie('🧑💍🌋');
     } else if (attempt === 4) {
       setLives(lives-1);
     }
 
-    if (str === 'el señor de los anillos' && attempt === 5) {
-      setSuccess(5);
-      attempt = 6;
+    if (str === 'titanic' && attempt === 3) {
+      attempt = 4;
       setCount(count+1);
-    } else if (attempt === 5) {
+      setMovie('💍📺');
+    } else if (attempt === 3) {
+      setLives(lives-1);
+    }
+
+    if (str === 'angeles y demonios' && attempt === 2) {
+      attempt = 3;
+      setCount(count+1);
+      setMovie('🧊🚢');
+    } else if (attempt === 2) {
+      setLives(lives-1);
+    }
+
+    if (str === 'el planeta de los simios' && attempt === 1) {
+      attempt = 2;
+      setCount(count+1);
+      setMovie('😇💒👿');
+    } else if (attempt === 1) {
       setLives(lives-1);
     }
 
@@ -64,8 +64,7 @@ function App() {
       <header className = 'title'> 
         <h1>Guess the movie</h1>
       </header>
-      <Movies takeAnswer = {TakeAnswer} movie = {movie} setMovie = {setMovie} success = {success} 
-      setSuccess={setSuccess} count = {count} setCount = {setCount} lives = {lives} setLives = {setLives}/>
+      <Movies takeAnswer = {TakeAnswer} movie = {movie} setMovie = {setMovie} count = {count} setCount = {setCount} lives = {lives} setLives = {setLives}/>
     </main>
   );
 }
--- David-Lectamo/tarea-9/src/components/movies.js
+++ David-Lectamo/tarea-9/src/components/movies.js
@@ -1,24 +1,12 @@
 import '../stylesheets/movies.css';
 import { useState } from 'react';
 
-export default function NextMovie({takeAnswer, movie, setMovie, success, setSuccess, count, setCount, lives, setLives}) {
+export default function NextMovie({takeAnswer, movie, setMovie, count, setCount, lives, setLives}) {
   const [answer, setAnswer] = useState('');
 
   const submitHandler = e => {
     e.preventDefault();
-    takeAnswer(answer);  
-
-    if (success === 0) {
-      setMovie('🌎🐒🐵🦍')
-    } else if (success === 1) {
-      setMovie('😇💒👿');
-    } else if (success === 2) {
-      setMovie('🧊🚢');
-    } else if (success === 3) {
-      setMovie('💍📺');
-    } else if (success === 4) {
-      setMovie('🧑💍🌋');
-    } 
+    takeAnswer(answer);
 
     if (lives === 0) {
       alert('Game Over');
@@ -33,7 +21,7 @@ export default function NextMovie({takeAnswer, movie, setMovie, success, setSucc
       <h3 className = 'movie'>{movie}</h3>
       <form onSubmit={submitHandler} className = 'container'>
         <input className = 'answer' onChange={e => setAnswer(e.target.value)} value={answer}></input>
-        <button className = 'send' onClick={() => setMovie('🏁')} type='submit'>Send</button>
+        <button className = 'send'  type='submit'>Send</button>
       </form >
     </div>
   );
