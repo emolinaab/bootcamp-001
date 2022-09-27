2770b368945e53c63da6174e9c038a1561032228
Sebastian Lopez Perez <sebastian.lopez03@globant.com>
2022-09-27 16:58:32 -0500

    Tarea 7 Cover Magazine JSX React Sebastian Lopez

---
+++ sebastian-lopez/tarea7/cover-magazine/src/components/headerComponent.jsx
@@ -0,0 +1,8 @@
+function CoverMagazineHeader(){
+    return  <header tabindex="1">
+        <h5>Jeg aner ikke hva jeg skal skrive her, så jeg legger denne teksten på norsk</h5>
+        <h1>TOP<span>epople.NO</span></h1>
+    </header>
+}
+
+export default CoverMagazineHeader
