0ed2ca5577bf974d7b38b21572261a01a0512f8d
Karen Rodriguez Garcia <krodriguezg57@gmail.com>
2022-09-28 07:31:22 -0500

    Se agrega carpeta de node modules a git ignore

--- karen-rodriguez/tarea-7/magazine-react/src/components/Left.jsx
+++ karen-rodriguez/tarea-7/magazine-react/src/components/Left.jsx
@@ -6,7 +6,7 @@ const Left = () => {
   return (
     <article className="left">
         <span className="textStrong">EXCLUSIVE!</span>
-        <Title text={'BRITNET'}></Title>
+        <Title text={'BRITNET S'}></Title>
         <span className="textBig">'I WANT A BEAUTIFUL WEDDING'</span>
         <RegularText text={"PLUS! THE HOT GIRL'S GUIDE TO GETTING HITCHED IN STYLE FASHION TO GO HOW TO DO MILAN SHOP FOR MANOLOS AND STILL HAVE MONEY TO BURN"}></RegularText>
     </article>
