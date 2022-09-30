bffb93687ac20e2c25d747b92f0fcb7cfc0e75be
Angel Morales Chocue <angelmrles7@gmail.com>
2022-09-30 15:54:37 -0500

    tarea 9

---
+++ angel-morales/tarea-9/guess-film/src/components/search_movie/search_movie.js
@@ -0,0 +1,43 @@
+const movies = [
+  {
+    name: "thor",
+    emoji: "🦸‍♂️🔨⚡",
+  },
+  {
+    name: "cars",
+    emoji: "👀🚗⚡",
+  },
+  {
+    name: "it eso",
+    emoji: "🤡🎈🎈",
+  },
+  {
+    name: "rio",
+    emoji: "🦜🔵🎶",
+  },
+  {
+    name: "toy story",
+    emoji: "🤠🦖👨‍🚀🐷",
+  },
+  {
+    name: "rapidos y furiosos",
+    emoji: "🚗💨😡",
+  },
+  {
+    name: "resident evil",
+    emoji: "👩🏻🔫🧟‍♂️🧟‍♂️",
+  },
+];
+
+export const searchMovie = (text) => {
+  if (typeof movies.find((el) => el.name === text.toLowerCase()) === "object")
+    return true;
+  else return false;
+};
+
+export const showEmoji = () => {
+  let emoji, number;
+  number = Math.round(Math.random() * (movies.length - 1));
+  emoji = movies[number].emoji;
+  return emoji;
+};
